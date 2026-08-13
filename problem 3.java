7f2377c707dd1551be427b395d930559e26976c9
Md Abdul Momit Mondol <251-15-464@diu.edu.bd>
2026-08-14 00:54:26 +0600

    Update problem 3.java

--- problem 3.java
+++ problem 3.java
@@ -1,3 +1,5 @@
+package com.mycompany.labreport;
+
 class BankAccount {
 
     private long balance = 0;
@@ -39,6 +41,41 @@ class BankAccount {
         if (amount <= 0 || amount > balance) {
             throw new IllegalArgumentException();
         }
+
+        balance -= amount;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        BankAccount account = new BankAccount();
+
+        try {
+            account.open();
+            System.out.println("Account opened.");
+
+            account.deposit(5000);
+            System.out.println("Balance: " + account.getBalance());
+
+            account.withdraw(2000);
+            System.out.println("Balance after withdraw: " + account.getBalance());
+
+            account.deposit(1000);
+            System.out.println("Balance after deposit: " + account.getBalance());
+
+            account.close();
+            System.out.println("Account closed.");
+
+        } catch (IllegalStateException e) {
+            System.out.println("Account is not open.");
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid amount.");
+        }
+    }
+}
 
         balance -= amount;
     }
